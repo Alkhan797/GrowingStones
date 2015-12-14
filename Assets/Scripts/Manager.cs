aa6a24557d1cc6660f0739b41ae6be906353930c
Alkhan797 <Alkhan797@gmail.com>
2015-12-14 02:42:51 +0100

    INIT

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+enum gameState
+{
+    START = 0,
+    RUN = 1,
+    END = 2
+}
+
+public class Manager : MonoBehaviour {
+
+    public int game_status;
+    public bool player_human;
+    public int score_j1;
+    public int score_j2;
+    public Dictionary<int, int> map;
+
+    public GameObject startMenu;
+    public GameObject mainUI;
+    public GameObject gameoverMenu;
+
+    private int taken_pawns_human;
+    private int taken_pawns_ia;
+
+    // Use this for initialization
+    void Start () {
+        game_status = 0;
+        score_j1 = 0;
+        score_j2 = 0;
+        player_human = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        startMenu.SetActive(false);
+        mainUI.SetActive(true);
+        game_status = 1;
+    }
+
+    void GameOver()
+    {
+        game_status = 2;
+        mainUI.SetActive(false);
+        gameoverMenu.SetActive(true);
+    }
+
+    public void Restart()
+    {SceneManager.LoadScene("Giban");}
+
+    public void Pass()
+    {player_human = !player_human;}
+
+    public void takePawn(int x, int y, bool human)
+    {
+    }
+}
---
+++ Assets/Scripts/tile.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tile : MonoBehaviour {
+
+    public GameObject white_stone;
+    public GameObject black_stone;
+    private Manager manager;
+    private GameObject stone;
+    private bool stone_set;
+    private float x;
+    private float z;
+    public int x_id;
+    public int y_id;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.Find("GameManager").GetComponent<Manager>();
+        x = gameObject.transform.position.x;
+        z = gameObject.transform.position.z;
+        stone_set = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTouchDown()
+    {
+        if (manager.game_status == 1)
+        {
+            if (!stone_set)
+            {
+                if (manager.player_human)
+                {
+                    stone = Instantiate(white_stone, new Vector3(x, 0.281f, z - 0.2f), Quaternion.identity) as GameObject;
+                    manager.Pass();
+                }
+                else if (!manager.player_human)
+                {
+                    stone = Instantiate(black_stone, new Vector3(x, 0.281f, z - 0.2f), Quaternion.identity) as GameObject;
+                    manager.Pass();
+                }
+                stone.transform.parent = gameObject.transform;
+                stone_set = true;
+            }
+        }
+    }
+
+    void OnTouchUp ()
+    {
+
+    }
+
+    void OnTouchStay ()
+    {
+
+    }
+}
